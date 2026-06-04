9350972a094b037809e74596dd505fb93df7ab8a
Ahbar <ahbar1234@gmail.com>
2026-06-04 13:42:01 +0530

    (progress) basics test

--- lib.rs
+++ lib.rs
@@ -7,9 +7,27 @@ pub mod node;
 #[cfg(test)]
 mod tests {
     use crate::{task, worker, manager, scheduler, node};
-    
+    use uuid::Uuid;
+    use std::time::{SystemTime, Instant};
+
     #[test]
     fn basic() {
-        
+        let mut t = task::Task::new(
+        Uuid::new_v4(),
+        "Task-1",
+        task::State::Pending,
+        "Image-1",
+        1024,
+        1);
+
+        let mut te = task::TaskEvent::new(
+            Uuid::new_v4(),
+            task::State::Pending,
+            SystemTime::now(), 
+            t.clone()
+        ); 
+
+        println!("task: {:?}", t);
+        println!("task event: {:?}", te); 
     }
 }
--- task/mod.rs
+++ task/mod.rs
@@ -6,32 +6,64 @@ use std::time::{SystemTime};
 // should zero sized marker types be used to state ??
 // I chose enums because there's too much common functionality between states and there
 // are a lot of other places whre states are being used to denote type rather than just transitions
-enum State {
-    PendingState,
+#[derive(Clone, Debug)]
+pub enum State {
+    Pending,
     Scheduled,
-    Runnin,
+    Running,
     Completed,
     Failed,
 }
 
-pub struct Task {
-    id:             Uuid,
-    name:           String,
-    state:          State,
-    image:          String,
-    memory:         usize,
-    disk:           usize,
-    exposed_ports:  HashMap<String, u16>,
-    port_bindings:  HashMap<String, String>,
-    restart_policy: String,
-    start_time:     SystemTime,
-    finish_time:    SystemTime,
+#[derive(Clone, Debug)]
+pub struct Task<'this> {
+    pub id:             Uuid,
+    pub name:           &'this str, // lifetime of 'this' task 
+    pub state:          State,
+    pub image:          &'this str,
+    pub memory:         usize,
+    pub disk:           usize,
+    pub exposed_ports:  HashMap<String, u16>,
+    pub port_bindings:  HashMap<String, String>,
+    pub restart_policy: Option<String>,
+    pub start_time:     Option<SystemTime>,
+    pub finish_time:    Option<SystemTime>,
 } 
 
-struct TaskEvent<State> {
+impl<'this> Task<'this> {
+    pub fn new(id: Uuid, name: &'this str, state: State, image: &'this str, memory: usize, disk: usize) -> Self {
+        Task {
+            id,
+            name,
+            state, 
+            image, 
+            memory, 
+            disk,
+            exposed_ports: HashMap::new(),
+            port_bindings: HashMap::new(),
+            restart_policy: None,
+            start_time:    None,
+            finish_time: None,
+        }
+    } 
+}
+
+#[derive(Debug, Clone)]
+pub struct TaskEvent<'this, State> {
     id:         Uuid,
     state:      State,
     timestamp:  SystemTime,
-    task:       Task,
+    task:       Task<'this>,
 } 
+
+impl<'this, State> TaskEvent<'this, State> {
+    pub fn new(id: Uuid, state: State, timestamp: SystemTime, task: Task<'this>) -> Self {
+        Self {
+            id, 
+            state, 
+            timestamp,
+            task
+        } 
+    } 
+}
 
